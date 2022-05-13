3a1c035584ea01dab2e14cce33158c6f300cd1ee
Hanysisko <tymek@radlowski.net>
2022-05-14 01:10:52 +0200

    grid and row components, with logic for green/yellow/grey letters + typing in currentGuess

---
+++ src/components/Row.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export default function Row({ currentGuess, guess }) {
+
+  //produces row with past guess
+  if(guess){
+    return(
+      <div className="row past">
+        {guess.map((letter, index) => (
+          
+          <div 
+            key={index} 
+            className={letter.color}
+          >
+            {letter.key}
+          </div>
+
+        ))}
+      </div>
+    )
+  }
+
+  //produces row with current guess, letter after letter
+  if(currentGuess){
+    let letters = currentGuess.split('');
+    return(
+      <div className="row current">
+        {letters.map((letter, index) => (
+          
+          <div key={index} className='filled'>{letter}</div>
+
+        ))}
+        {/* this one is for "dummy" boxes, at the end of currentGuess */}
+        {[...Array(5 - letters.length)].map((_, index) => (
+          <div key={index} className='filled'></div>         
+        ))}
+      </div>
+    )
+  }
+
+  return(
+    <div className="row">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+};
--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
+import Grid from "./Grid";
 
 export default function Wordle({ solution }) {
   const { currentGuess, guesses, isCorrect, turn, handleKeyup } = useWordle(solution);
@@ -12,9 +13,19 @@ export default function Wordle({ solution }) {
   useEffect(() => {
     console.log(guesses, isCorrect, turn)
   }, [turn])
-  
+   
   return (
-    <div>Wordle solution is {solution} and current guess is |{currentGuess}|</div>
+    <div>
+      <div>Wordle solution is {solution} and current guess is |{currentGuess}|</div>
+      
+      <Grid 
+        currentGuess={currentGuess}
+        guesses={guesses}
+        turn={turn}
+      />
+
+    </div>
+
 
   );
 }
